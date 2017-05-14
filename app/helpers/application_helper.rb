274fb10dc6371d20280ebf1c476cf29c6fa4a290
Igor <siafin2010@gmail.com>
2017-05-14 23:16:25 +0300

    v

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fa_link_to(icon, *args, &block)
+  	link_to(*args) do
+  		concat content_tag("span", nil, class: "fa fa#{icon}")
+  		concat ' '
+  		concat capture &block
+  	end
+  end	
 end
