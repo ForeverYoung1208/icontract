4032ee427b19ba119534c476efb0a6bda249f45d
Igor <siafin2010@gmail.com>
2017-10-18 19:10:03 +0300

    a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
 
   def mainmenu_item( p, *args, &block)
   	mainmenu_li(p, class: "nav-item") do
-  		link_to( p[:menuitem], class: 'nav-link' ) do
+  		link_to( p[:menuitem], class: "nav-link #{'disabled' if !p[:active]}" ) do
   			capture &block
   		end
   	end
@@ -43,7 +43,13 @@ module ApplicationHelper
       }) { capture &block } +
       content_tag("div", { class: "dropdown-menu", 'aria-labelledby': "navbarDropdownMenuLink1" }) do 
         p[:menuitems].collect do |name, path|
-          concat( link_to( path, class: 'dropdown-item' ){ name } )
+          if path[1] == true #active
+            concat( link_to( path[0], class: "dropdown-item" ){ name } )
+          else #not active
+            concat( tag.span( class: "dropdown-item disabled" ){ name } )
+          end
+
+            
         end
       end
 
