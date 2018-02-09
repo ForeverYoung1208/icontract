cd22653d2733b57af56e9dd1030d0725ecac5039
Igor <siafin2010@gmail.com>
2018-02-09 19:02:58 +0200

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,9 +55,15 @@ config.serve_static_assets = true
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "icontract_#{Rails.env}"
+###
+  config.active_job.queue_adapter     = :resque
+  config.active_job.queue_name_prefix = "icontract_#{Rails.env}"
+###
+
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
